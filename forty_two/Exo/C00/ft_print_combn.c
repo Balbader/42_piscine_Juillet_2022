5ad2ac77704bcd3b5f250e3a3057ce55a2fcc41f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-24 11:34:53 +0200

    update

---
+++ forty_two/Exo/C00/ft_print_combn.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+void	ft_putchar(char c);
+
+int	main(void)
+{
+	printf("%d\n", 3^3);
+	return (0);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
