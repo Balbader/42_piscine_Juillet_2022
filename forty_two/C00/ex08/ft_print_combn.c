1593d9fdf7cb91cc0dcf23712403f1d95f623a70
Balbader <15971200+Balbader@users.noreply.github.com>
2021-10-25 13:49:58 +0200

    update

--- forty_two/C00/ex08/ft_print_combn.c
+++ forty_two/C00/ex08/ft_print_combn.c
@@ -1,15 +1,39 @@
 #include <unistd.h>
-#include <stdio.h>
 
+void	ft_print_combn(int n);
 void	ft_putchar(char c);
+void	ft_special_char(void);
+void 	ft_check_n(int n);
+
 
 int	main(void)
 {
-	printf("%d\n", 3^3);
+	ft_print_combn(12);
 	return (0);
 }
 
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
+}
+
+void	ft_print_combn(int n)
+{
+	ft_check_n(n);
+}
+
+void 	ft_check_n(int n)
+{
+	if (n < 0 || n > 9)
+		return ;
+	if (n == 1)
+		ft_putchar('1');
+	if (n == 9)
+		write(1, "123456789", 9);
+}
+
+void	ft_special_char(void)
+{
+	ft_putchar(',');
+	ft_putchar(' ');
 }
