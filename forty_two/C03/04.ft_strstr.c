507674cf00e117f670985d03dee1f2024326b376
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-01 13:20:29 +0100

    04.ft_strstr.c

--- forty_two/C03/04.ft_strstr.c
+++ forty_two/C03/04.ft_strstr.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+char	*ft_strstr(char *str, char *to_find);
+
 int main(void)
 {
+
 	return (0);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	if (to_find == " ")
+		return (str);
+	return (to_find);
 }
