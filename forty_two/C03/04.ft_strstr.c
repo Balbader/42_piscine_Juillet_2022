faa13620c4379fa3cf6f5af4087b4e620accb6c9
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-06 13:50:48 +0100

    update

--- forty_two/C03/04.ft_strstr.c
+++ forty_two/C03/04.ft_strstr.c
@@ -8,7 +8,7 @@ int		ft_strlen(char *str);
 int main(void)
 {
 	char	str[101] = "World Hello Hello world Hello";
-	char	needle[101] = "Hello";
+	char	needle[101] = "";
 
 	printf("ft_strstr: %s\n", ft_strstr(str, needle));
 	printf("strstr: %s\n", strstr(str, needle));
@@ -16,34 +16,34 @@ int main(void)
 	return (0);
 }
 
-char	*ft_strstr(char *str, char *to_find)
-{
-	int		i;
-	int		j;
-	int		count;
+/*char	*ft_strstr(char *str, char *to_find)*/
+/*{*/
+	/*int		i;*/
+	/*int		j;*/
+	/*int		count;*/
 
-	count = 0;
-	i = 0;
-	if (*to_find == '\0')
-		return(str);
-	while (str[i])
-	{
-		j = 0;
-		while (to_find[j])
-		{
-			if (to_find[j] == str[i])
-				count++;
-			j++;
-		}
-		i++;
-	}
-	if (count == 0)
-		return (NULL);
-	printf("%d\n", j);
-	if (count > 0)
-		ft_strcpy(i, to_find, str);
-	return (to_find);
-}
+	/*count = 0;*/
+	/*i = 0;*/
+	/*if (*to_find == '\0')*/
+		/*return(str);*/
+	/*while (str[i])*/
+	/*{*/
+		/*j = 0;*/
+		/*while (to_find[j])*/
+		/*{*/
+			/*if (to_find[j] == str[i])*/
+				/*count++;*/
+			/*j++;*/
+		/*}*/
+		/*i++;*/
+	/*}*/
+	/*if (count == 0)*/
+		/*return (NULL);*/
+	/*printf("%d\n", j);*/
+	/*if (count > 0)*/
+		/*ft_strcpy(i, to_find, str);*/
+	/*return (to_find);*/
+/*}*/
 
 char	*ft_strcpy(int i, char *dest, char *src)
 {
@@ -66,4 +66,26 @@ int		ft_strlen(char *str)
 	while (str[i])
 		i++;
 	return (i);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	unsigned int i;
+	unsigned int j;
+
+	i = 0;
+	if (!to_find[0])
+		return (str);
+	while (str[i])
+	{
+		j = 0;
+		while (str[i + j] == to_find[j])
+		{
+			j++;
+			if (!to_find[j])
+				return (str + i);
+		}
+		i++;
+	}
+	return (0);
 }
