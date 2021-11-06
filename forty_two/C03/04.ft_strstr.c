29047fd8a23afe6c2814e7619ea2045810a8893b
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-06 13:58:04 +0100

    update

--- forty_two/C03/04.ft_strstr.c
+++ forty_two/C03/04.ft_strstr.c
@@ -2,70 +2,16 @@
 #include <string.h>
 
 char	*ft_strstr(char *str, char *to_find);
-char	*ft_strcpy(int i, char *dest, char *src);
-int		ft_strlen(char *str);
 
 int main(void)
 {
 	char	str[101] = "World Hello Hello world Hello";
-	char	needle[101] = "";
+	char	needle[101] = "llo";
 
 	printf("ft_strstr: %s\n", ft_strstr(str, needle));
 	printf("strstr: %s\n", strstr(str, needle));
 
 	return (0);
-}
-
-/*char	*ft_strstr(char *str, char *to_find)*/
-/*{*/
-	/*int		i;*/
-	/*int		j;*/
-	/*int		count;*/
-
-	/*count = 0;*/
-	/*i = 0;*/
-	/*if (*to_find == '\0')*/
-		/*return(str);*/
-	/*while (str[i])*/
-	/*{*/
-		/*j = 0;*/
-		/*while (to_find[j])*/
-		/*{*/
-			/*if (to_find[j] == str[i])*/
-				/*count++;*/
-			/*j++;*/
-		/*}*/
-		/*i++;*/
-	/*}*/
-	/*if (count == 0)*/
-		/*return (NULL);*/
-	/*printf("%d\n", j);*/
-	/*if (count > 0)*/
-		/*ft_strcpy(i, to_find, str);*/
-	/*return (to_find);*/
-/*}*/
-
-char	*ft_strcpy(int i, char *dest, char *src)
-{
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-int		ft_strlen(char *str)
-{
-	int		i;
-	int		j;
-
-	j = 0;
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
 }
 
 char	*ft_strstr(char *str, char *to_find)
