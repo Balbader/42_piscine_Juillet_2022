cf0bd4228552f2e61d6e03e08e4efc1628e120df
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-06 15:41:38 +0100

    update

--- forty_two/C04/02.ft_putnbr.c
+++ forty_two/C04/02.ft_putnbr.c
@@ -5,15 +5,23 @@ void	ft_putchar(char c);
 
 int	main(void)
 {
+	ft_putnbr(-2147483647);
 	return (0);
 }
 
 void	ft_putnbr(int nb)
 {
-	if (nb == -2147483648)
+	if (nb < 0)
 	{
 		ft_putchar('-');
-		nb = -nb;
+		ft_putnbr(nb * -1);
+	}
+	else if (nb <= 9)
+		ft_putchar(nb + 48);
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(48 + (nb % 10));
 	}
 }
 
