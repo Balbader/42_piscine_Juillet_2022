3c2e06f8d5e6339e64b2e2b5af0c2a411a64fdf7
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-08 12:56:16 +0100

    update info

--- The Book/Chapter 1/2. celsius_to_fahrenheit.c
+++ The Book/Chapter 1/2. celsius_to_fahrenheit.c
@@ -15,13 +15,14 @@ int	main(void)
 	upper = 300;
 	step = 20;
 	fahr = lower;
+	printf("Fahr\tCel\n");
 	while (fahr <= upper)
 	{
 		/*
 		 * We were unable to use 5/9 int the previous version because integers
 		 * division would truncate it to zero
 		 * celsius = (5 * (fahr - 32)) / 9;
-		*/
+		 */
 		celsius = (5.0 / 9.0) * (fahr - 32.0);
 		printf("%3.0f\t%6.1f\n", fahr, celsius);
 		fahr += step;
