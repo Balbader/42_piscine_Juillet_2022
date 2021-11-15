2856b8def71672e21ae0957e28c3fa53cfd62ac5
Balbader <15971200+Balbader@users.noreply.github.com>
2021-11-15 16:57:48 +0100

    update

--- The Book/Chapter 1/1-10. replace_tabs_backspace_backslash.c
+++ The Book/Chapter 1/1-10. replace_tabs_backspace_backslash.c
@@ -2,5 +2,20 @@
 
 int	main(void)
 {
+	int	c;
+
+	c = getchar();
+	while (c != EOF)
+	{
+		c = getchar();
+		if (c == '\t')
+			printf("\\t\n");
+		else if (c == '\b')
+			printf("\\b\n");
+		else if (c == '\\')
+			printf("\\\\\n");
+		else
+			printf("%c\n", c);
+	}
 	return (0);
 }
