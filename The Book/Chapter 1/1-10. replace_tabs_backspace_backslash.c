904947fb516bf7120f6463a39fac62405f857592
Balbader <15971200+Balbader@users.noreply.github.com>
2022-03-21 14:29:12 +0100

    update linting

--- The Book/Chapter 1/1-10. replace_tabs_backspace_backslash.c
+++ The Book/Chapter 1/1-10. replace_tabs_backspace_backslash.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-int	main(void)
-{
-	int	c;
+int main(void) {
+  int c;
 
-	c = getchar();
-	while (c != EOF)
-	{
-		c = getchar();
-		if (c == '\t')
-			printf("\\t\n");
-		else if (c == '\b')
-			printf("\\b\n");
-		else if (c == '\\')
-			printf("\\\\\n");
-		else
-			printf("%c\n", c);
-	}
-	return (0);
+  c = getchar();
+
+  while (c != EOF) {
+    c = getchar();
+    if (c == '\t')
+      printf("\\t\n");
+    else if (c == '\b')
+      printf("\\b\n");
+    else if (c == '\\')
+      printf("\\\\\n");
+    else
+      printf("%c\n", c);
+  }
+  return (0);
 }
