78d004c0242f6e0bd3f197c553327555c41944c7
balou <baloucode83@gmail.com>
2022-04-13 11:17:05 +0200

    name update

---
+++ forty_two_prep/C00/ex08/ft_print_combn.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    ft_print_seperators(void)
+{
+    ft_putchar(',');
+    ft_putchar(' ');
+}
+
+void    ft_print_combn(int n)
+{
+
+}
+
+int     main(void)
+{
+    ft_print_combn(1);
+    ft_putchar('\n');
+    ft_print_combn(2);
+    ft_putchar('\n');
+    ft_print_combn(3);
+    ft_putchar('\n');
+    ft_print_combn(4);
+    ft_putchar('\n');
+    ft_print_combn(5);
+    ft_putchar('\n');
+    ft_print_combn(6);
+    ft_putchar('\n');
+    ft_print_combn(7);
+    ft_putchar('\n');
+    ft_print_combn(8);
+    ft_putchar('\n');
+    ft_print_combn(9);
+    return (0);
+}
