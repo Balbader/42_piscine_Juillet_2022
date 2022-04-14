c31dbf8458c887d3f80608f6715b30263ea9872c
balou <baloucode83@gmail.com>
2022-04-14 07:52:53 +0200

    ft_putnbr.c

--- forty_two_prep/C00/ex07/ft_putnbr.c
+++ forty_two_prep/C00/ex07/ft_putnbr.c
@@ -5,39 +5,23 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void    ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    if (nb == -2147483647)
+    if (nb == -2147483648)
     {
-        write(1, "-2147483647", 11);
-        return ;
+        write(1, "-2147483648", 11);
+        return;
     }
-    if (nb < 0)
-    {
-        ft_putchar('-');
-        ft_putnbr(nb * -1);
-        return ;
-    }
-    if (nb <= 9)
-        ft_putchar(nb + '0');
-    else
-    {
-        ft_putnbr(nb / 10);
-        ft_putchar(nb % 10 + '0');
-    }
-
-}
-
-int     main(void)
-{
-    ft_putnbr(-2147483647);
-    write(1, "\n", 1);
-    ft_putnbr(2147483647);
-    write(1, "\n", 1);
-    ft_putnbr(-12);
-    write(1, "\n", 1);
-    ft_putnbr(112);
-    write(1, "\n", 1);
-    ft_putnbr(4);
-    return (0);
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar(nb % 10 + '0');
+	}
+	else
+		ft_putchar(nb + '0');
 }
