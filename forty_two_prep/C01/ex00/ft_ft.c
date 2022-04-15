137024089b1298737b819dfec16b478b45e00800
balou <baloucode83@gmail.com>
2022-04-15 12:38:17 +0200

    update file

--- forty_two_prep/C01/ex00/ft_ft.c
+++ forty_two_prep/C01/ex00/ft_ft.c
@@ -1,5 +1,21 @@
 #include <unistd.h> 
 
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
 void    ft_ft(int *ptr)
 {
+    *ptr = 42;
+    ft_putchar(*ptr);
+}
+
+int     main(void)
+{
+    int     nb;
+    
+    nb = 22;
+    ft_ft(nb);
+    return (0);
 }
