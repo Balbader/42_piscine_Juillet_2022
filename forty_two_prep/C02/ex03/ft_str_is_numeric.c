bc23143a3c42c69d2bf169142869d298b4627556
balou <baloucode83@gmail.com>
2022-04-19 12:27:58 +0200

    ft_str_is_numeric.c

---
+++ forty_two_prep/C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,19 @@
+int     ft_str_is_numeric(char *str)
+{
+    int     i;
+
+    i = 0;
+    if (str[i] == '\0')
+        return (1);
+    else
+    {
+        while (str[i] != '\0')
+        {
+            if (!(str[i] >= '0' && str[i] <= '9'))
+                return (0);
+            else
+                i++;
+        }
+    }
+    return (1);
+}
