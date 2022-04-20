d79a534d843eac71cd1da52814c3a0ba04c30781
balou <baloucode83@gmail.com>
2022-04-20 15:31:27 +0200

    ft_strcapitalize.c

--- forty_two_prep/C02/ex09/ft_strcapitalize.c
+++ forty_two_prep/C02/ex09/ft_strcapitalize.c
@@ -10,7 +10,20 @@ void    ft_to_upr(char *c)
         *c -= 32;
 }
 
-int     ft_check_non_alpha(char *c)
+void    ft_str_to_lwr(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        i++;
+    }
+}
+
+int     ft_check_if_non_alpha(char *c)
 {
     if ((*c >= 'a' && *c <= 'z')
             || (*c >= 'A' && *c <= 'Z')
@@ -24,11 +37,13 @@ char    *ft_strcapitalize(char *str)
     int     i;
 
     i = 0;
+    ft_str_to_lwr(str);
     if (str[i] >= 'a' && str[i] <= 'z')
         ft_to_upr(&str[i]);
     while (str[i] != '\0')
     {
-        if (ft_check_non_alpha(&str[i - 1]) && !ft_check_non_alpha(&str[i]))
+        if (ft_check_if_non_alpha(&str[i - 1])
+                && !ft_check_if_non_alpha(&str[i]))
             ft_to_upr(&str[i]);
         i++;
     }
