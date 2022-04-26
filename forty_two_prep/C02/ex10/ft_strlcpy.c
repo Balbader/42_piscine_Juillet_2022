5724e45c7e4cb62af210b781439c43edda82f28b
balou <baloucode83@gmail.com>
2022-04-26 19:54:29 +0200

    update

--- forty_two_prep/C02/ex10/ft_strlcpy.c
+++ forty_two_prep/C02/ex10/ft_strlcpy.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
+#include <string.h>
 
-unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
-{
-
-}
+//unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
 
 int     main(void)
 {
+    char            src[] = "World";
+    char            dst[] = "";
+    size_t          size;
+    int    res;
+
+    size = 3;
+    res = strlcpy(dst, src, size);
+    printf("%u\n", res);
     return (0);
 }
