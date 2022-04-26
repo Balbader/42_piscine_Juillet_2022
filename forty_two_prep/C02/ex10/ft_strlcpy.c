dc12833820bd975eaaebde06bd335ce4651f54a4
balou <baloucode83@gmail.com>
2022-04-26 20:14:05 +0200

    update

--- forty_two_prep/C02/ex10/ft_strlcpy.c
+++ forty_two_prep/C02/ex10/ft_strlcpy.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
-#include <string.h>
+#include <bsd/string.h>
 
-//unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+
+}
+
+void test(int size)
+{
+    char string[] = "Hello there, Venus";
+    char buffer[19];
+    int r;
+
+    r = strlcpy(buffer,string,size);
+
+    printf("Copied '%s' into '%s', length %d\n",
+            string,
+            buffer,
+            r
+          );
+}
 
 int     main(void)
 {
-    char            src[] = "World";
-    char            dst[] = "";
-    size_t          size;
-    int    res;
-
-    size = 3;
-    res = strlcpy(dst, src, size);
-    printf("%u\n", res);
+    test(19);
+    test(10);
+    test(1);
+    test(0);
     return (0);
 }
