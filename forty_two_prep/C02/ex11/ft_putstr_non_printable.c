95fa74f30800c5bd0a5d6c6960ca0a0b04dea5f0
balou <baloucode83@gmail.com>
2022-04-28 08:04:56 +0200

    ft_putstr_non_printable.c

--- forty_two_prep/C02/ex11/ft_putstr_non_printable.c
+++ forty_two_prep/C02/ex11/ft_putstr_non_printable.c
@@ -5,7 +5,7 @@ void    ft_putchar(char c)
     write(1, &c, 1);
 }
 
-void    ft_make_hex_printable(unsigned char nb)
+void    ft_make_hex_printable(int nb)
 {
     char    hex[] = "0123456789abcdef";
 
@@ -16,13 +16,19 @@ void    ft_make_hex_printable(unsigned char nb)
 void    ft_putstr_non_printable(char *str)
 {
     int     i;
+    char    c;
 
     i = 0;
-}
-
-int     main(void)
-{
-    char    str[] = "Coucou\ntu vas\tbien ?";
-    ft_putstr_non_printable(str);
-    return (0);
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 32 && str[i] <= 126)
+            ft_putchar(str[i]);
+        else
+        {
+            c = '\\';
+            ft_putchar(c);
+            ft_make_hex_printable(str[i]);
+        }
+        i++;
+    }
 }
