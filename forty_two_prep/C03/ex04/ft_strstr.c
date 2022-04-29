86fe3ee40a085519a8d95119be7512b95276b93d
balou <baloucode83@gmail.com>
2022-04-29 22:41:22 +0200

    word in progress

--- forty_two_prep/C03/ex04/ft_strstr.c
+++ forty_two_prep/C03/ex04/ft_strstr.c
@@ -1,54 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 
-char    *ft_strcpy(char *dest, char *src)
+char    *ft_strstr(char *str, char *to_find)
 {
     int     i;
 
     i = 0;
-    while (src[i] != '\0')
+    while(str[i])
     {
-        dest[i] = src[i];
+        if (str[i] == to_find[i])
+            return (str);
         i++;
     }
-    dest[i] = '\0';
-    return (dest);
+    return (str);
 }
-
-void    ft_strstr(char *str, char *to_find)
-{
-    char    temp[] = "";
-    int     i;
-    int     j;
-    int     k;
-
-    i = 0;
-    j = 0;
-    k = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] == to_find[j])
-        {
-            while (str[i] != '\0')
-            {
-                temp[k] = str[i];
-                k++;
-                i++;
-            }
-        }
-        i++;
-        j++;
-    }
-    printf("%s\n", temp);
-}
-
 
 int     main(void)
 {
     char    a[] = "Hello My name is Balou";
     char    b[256] = "B";
 
-    ft_strstr(a, b);
-    printf("%s\n", ft_strcpy(a, b));
+    printf("%s\n", ft_strstr(a, b));
     return (0);
 }
