362e159b676887263c138eaba8011ba298aa6bfe
balou <baloucode83@gmail.com>
2022-04-30 18:36:57 +0200

    ft_strstr.c

--- forty_two_prep/C03/ex04/ft_strstr.c
+++ forty_two_prep/C03/ex04/ft_strstr.c
@@ -1,49 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-char    *ft_strcpy(char *src, char *dest)
-{
-    unsigned int    i;
-
-    i = 0;
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    return (dest);
-}
-
 char    *ft_strstr(char *str, char *to_find)
 {
-    unsigned int    i;
-    unsigned int    j;
-    char            temp[256];
+    unsigned int i;
+    unsigned int j;
 
     i = 0;
-    j = 0;
     while (str[i])
     {
-        if (str[i] == to_find[j])
+        j = 0;
+        while (str[i] == to_find[j])
         {
-            while (str[j])
-            {
-                temp[j] = str[j];
-                j++;
-            }
-            break ;
+            j++;
+            if (str[i] != to_find[j])
+                return (str + i);
         }
         i++;
     }
-    printf("temp: %s\n", temp);
-    ft_strcpy(temp, str);
-    return (str);
+    return (0);
 }
 
 int     main(void)
 {
     char    a[] = "Hello My name is Balou";
-    char    b[256] = "a";
+    char    b[256] = "y";
     char    *res;
 
     res = ft_strstr(a, b);
