86472989a0ad80f93defec310186a10fddd23b70
balou <baloucode83@gmail.com>
2022-05-01 12:50:46 +0200

    ft_strstr.c version 2

---
+++ forty_two_prep/C03/ex04/ft_strstr_v2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+unsigned int     ft_strlen(char *str)
+{
+    unsigned int     i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
+char    *ft_strstr(char *str, char *to_find)
+{
+    unsigned int    i;
+    unsigned int    j;
+
+    i = 0;
+    j = 0;
+    while (i < ft_strlen(str))
+    {
+        if (str[i] == to_find[j])
+        {
+            j++;
+            if (j == ft_strlen(to_find))
+                return (&str[i - ft_strlen(to_find) + 1]);
+        }
+        else
+            j = 0;
+        i++;
+    }
+    return (0);
+}
+
+int     main(void)
+{
+    char    a[] = "Hello my name is balou";
+    char    b[] = "y";
+    printf("%s\n", ft_strstr(a, b));
+    return (0);
+}
