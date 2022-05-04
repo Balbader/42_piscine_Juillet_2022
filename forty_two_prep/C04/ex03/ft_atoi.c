b5246593e609a00021c6a268a22ac6540a73cf79
balou <baloucode83@gmail.com>
2022-05-04 09:25:04 +0200

    ft_atoi.c

--- forty_two_prep/C04/ex03/ft_atoi.c
+++ forty_two_prep/C04/ex03/ft_atoi.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int     ft_atoi(char *str)
-{
-    return (0);
-}
+//int     ft_atoi(char *str)
 
 int     main(void)
 {
+    char    str[] = "   ---+--+1234ab567";
+    int     res;
+
+    res = atoi(str);
+    printf("%s\n", str);
+    printf("%d\n", res);
     return (0);
 }
