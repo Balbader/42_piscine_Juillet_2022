ca985d3081d569d0403a44cd9d00f3c8dead9667
balou <baloucode83@gmail.com>
2022-05-04 21:57:23 +0200

    ft_atoi.c

--- forty_two_prep/C04/ex03/ft_atoi.c
+++ forty_two_prep/C04/ex03/ft_atoi.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int     ft_atoi(char *str)
 {
@@ -7,5 +8,14 @@ int     ft_atoi(char *str)
 
 int     main(void)
 {
+    int     sys;
+    int     me;
+    char    str[] = "12345";
+
+    sys = atoi(str);
+    me = ft_atoi(str);
+    printf("str = %s\n", str);
+    printf("sys: %d\n", sys);
+    printf("me: %d\n", me);
     return (0);
 }
