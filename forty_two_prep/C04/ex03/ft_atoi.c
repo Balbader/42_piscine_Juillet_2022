6e341a6cb75618e3bbb0f550a245f3680140894e
balou <baloucode83@gmail.com>
2022-05-05 06:12:03 +0200

    ft_atoi.c

--- forty_two_prep/C04/ex03/ft_atoi.c
+++ forty_two_prep/C04/ex03/ft_atoi.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 1. Delete all white space
+// 2. Delete all + signs
+// 3. Count all (-). If count is even returned int will be positive. If odd returned int will be negative.
+// 4. Int will be base 10: 0123456789
+// 5. The returned int should only store all possible values of int: [−2,147,483,647, +2,147,483,647] range.
+
+
+
 int     ft_atoi(char *str)
 {
+    int     i;
 
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == ' ')
+            i++;
+
+        i++;
+    }
 }
 
 int     main(void)
