a1348fe5d35e8cf7eb66e43f93f9089780dd3a3e
balou <baloucode83@gmail.com>
2022-05-06 18:57:55 +0200

    ft_atoi.c

--- forty_two_prep/C04/ex03/ft_atoi.c
+++ forty_two_prep/C04/ex03/ft_atoi.c
@@ -1,14 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-// 1. Pass all white space
-// 2. Pass all + signs
-// 3. Count all (-). If count is even returned int will be positive.
-//    If odd returned int will be negative.
-// 4. Int will be base 10: 0123456789
-// 5. The returned int should only store all possible values 
-//    of int: [−2,147,483,647, +2,147,483,647] range.
-
 int     ft_check_if_nbr(char c)
 {
     if (c >= '0' && c <= '9')
@@ -43,30 +32,19 @@ int     ft_atoi(char *str)
     i = 0;
     while (str[i] != '\0')
     {
-        if (str[i] == ' ' || str[i] == '+' || str[i] == '-')
+        while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+                || str[i] == '\f' || str[i] == '\r' || str[i] == ' '
+                || str[i] == '+' || str[i] == '-')
             i++;
-        else if ((ft_check_if_nbr(str[i])))
-        {
-            printf("str[i]: %c\n", str[i]);
+        if (ft_check_if_nbr(str[i]))
             res = res * 10 + str[i] - '0';
-            printf("res: %d\n", res);
-        }
         else
         {
-            if (ft_count_operators(str) == 0)
+            if (!ft_count_operators(str))
                 res *= -1;
             return (res);
         }
         i++;
     }
-    return (0);
-}
-
-int     main(void)
-{
-    char    str[] = "-----123ase75";
-
-    printf("str = %s\n", str);
-    printf("ft_atoi: %d\n", ft_atoi(str));
     return (0);
 }
