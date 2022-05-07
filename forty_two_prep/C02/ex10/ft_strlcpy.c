98ef058fc621db7c6567527d2db1e16ef6fa5961
balou <baloucode83@gmail.com>
2022-05-07 13:08:25 +0200

    ft_strlcpy.c

--- forty_two_prep/C02/ex10/ft_strlcpy.c
+++ forty_two_prep/C02/ex10/ft_strlcpy.c
@@ -1,6 +1,6 @@
-int     ft_strlen(char *str)
+unsigned int    ft_strlen(char *str)
 {
-    int     i;
+    unsigned int i;
 
     i = 0;
     while (str[i] != '\0')
@@ -10,8 +10,8 @@ int     ft_strlen(char *str)
 
 unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-    unsigned int    i;
-    int             src_len;
+    unsigned int i;
+    unsigned int src_len;
 
     src_len = ft_strlen(src);
     i = 0;
