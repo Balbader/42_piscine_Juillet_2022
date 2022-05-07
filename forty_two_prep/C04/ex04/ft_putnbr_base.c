0fb5a5e708b177634560968a63ab8e54f6e0785a
balou <baloucode83@gmail.com>
2022-05-07 11:05:14 +0200

    ft_putnbr_base.c

--- forty_two_prep/C04/ex04/ft_putnbr_base.c
+++ forty_two_prep/C04/ex04/ft_putnbr_base.c
@@ -1,5 +1,15 @@
 #include <unistd.h>
 
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
 void    ft_putnbr(int nbr, char *base)
 {
 }
