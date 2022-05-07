d358b9feda66a62f5da99bcb77ab17968d246ca2
balou <baloucode83@gmail.com>
2022-05-07 12:41:54 +0200

    ft_putnbr_base.c

--- forty_two_prep/C04/ex04/ft_putnbr_base.c
+++ forty_two_prep/C04/ex04/ft_putnbr_base.c
@@ -7,7 +7,35 @@ int     ft_strlen(char *str)
     i = 0;
     while (str[i])
         i++;
-    return (i);
+    return (i);;
+}
+
+int     ft_check_base(char *base)
+{
+    unsigned int    i;
+    unsigned int    j;
+
+    if (ft_strlen(base) < 2)
+        return (0);
+    while (base[i])
+    {
+        if (base[i] == '+' || base[i] == '-')
+            return (0);
+        i++;
+    }
+    i = 0;
+    while (base[i])
+    {
+        j = i + 1;
+        while (base[j])
+        {
+            if (base[i] == base[j])
+                return (0);
+            j++;
+        }
+        i++;
+    }
+    return (1);
 }
 
 void    ft_putnbr(int nbr, char *base)
