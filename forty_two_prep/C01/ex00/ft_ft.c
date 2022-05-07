499fd0bb5dac84220631eca6b05c9abdaf317e97
balou <baloucode83@gmail.com>
2022-05-07 12:53:05 +0200

    ft_ft.c

--- forty_two_prep/C01/ex00/ft_ft.c
+++ forty_two_prep/C01/ex00/ft_ft.c
@@ -1,18 +1,4 @@
-#include <unistd.h> 
-#include <stdio.h>
-
 void    ft_ft(int *nbr)
 {
     *nbr = 42;
-}
-
-int     main(void)
-{
-    int     ptr;
-
-    ptr = 88;
-    printf("before: %d\n", ptr);
-    ft_ft(&ptr);
-    printf("after: %d\n", ptr);
-    return (0);
 }
