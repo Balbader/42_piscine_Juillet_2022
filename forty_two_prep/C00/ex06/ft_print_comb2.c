13e8c397e8a131a1a14ceddf3b9ba14205d50494
Balbader <15971200+Balbader@users.noreply.github.com>
2022-05-08 10:26:48 +0200

    ft_print_comb2.c

--- forty_two_prep/C00/ex06/ft_print_comb2.c
+++ forty_two_prep/C00/ex06/ft_print_comb2.c
@@ -16,13 +16,15 @@ void    ft_print_numbers(int a, int b)
 
 void    ft_print_comb2(void)
 {
-  int a;
-  int b;
+  int   a;
+  int   b;
 
   a = 0;
-  while (a <= 98) {
+  while (a <= 98)
+  {
     b = a + 1;
-    while (b <= 99) {
+    while (b <= 99)
+    {
       ft_print_numbers(a, b);
       if (a != 98 || b != 99)
         write(1, ", ", 2);
