a6354bac4ace587efe6abb49f0bc64291daf4cb9
balou <baloucode83@gmail.com>
2022-05-10 21:37:38 +0200

    ft_recursive_power.c

--- forty_two_prep/C05/ex03/ft_recursive_power.c
+++ forty_two_prep/C05/ex03/ft_recursive_power.c
@@ -1,20 +1,8 @@
-#include <stdio.h>
-
 int     ft_recursive_power(int nb, int power)
 {
     if (power < 0)
         return (0);
     if ((nb == 0 && power == 0) || power == 0)
         return (1);
-
-}
-
-int     main(void)
-{
-    printf("%d\n", ft_recursive_power(0, 0));
-    printf("%d\n", ft_recursive_power(5, 0));
-    printf("%d\n", ft_recursive_power(0, 4));
-    printf("%d\n", ft_recursive_power(5, -3));
-    printf("%d\n", ft_recursive_power(5, 3));
-    return (0);
+    return (nb * ft_recursive_power(nb, power - 1));
 }
