a321bd146293fd4e4dc9b9cfa7183520e407a2b3
balou <baloucode83@gmail.com>
2022-05-10 14:24:25 +0200

    ft_iterative_factorial.c

--- forty_two_prep/C05/ex00/ft_iterative_factorial.c
+++ forty_two_prep/C05/ex00/ft_iterative_factorial.c
@@ -1,16 +1,14 @@
 int     ft_iterative_factorial(int nb)
 {
-    int     res;
     int     i;
 
     i = 1;
-    res = 1;
-    if (nb < 0 || nb != sizeof(int))
+    if (nb < 0)
         return (0);
-    while (i <= nb)
+    while (nb > 0)
     {
-        res *= i;
-        i++;
+        i *= nb;
+        nb--;
     }
-    return (res);
+    return (i);
 }
