03c682674cd00d07ffd5c670e94955fc83c89717
balou <baloucode83@gmail.com>
2022-05-12 18:31:38 +0200

    ft_find_next_prime.c

--- forty_two_prep/C05/ex07/ft_find_next_prime.c
+++ forty_two_prep/C05/ex07/ft_find_next_prime.c
@@ -1,11 +1,31 @@
-#include <stdio.h>
+int     ft_check_if_prime(int nb)
+{
+    int     i;
+    int     count;
+
+    count = 0;
+    i = 1;
+    while (i <= nb)
+    {
+        if (nb % i == 0)
+            count += 1;
+        i++;
+    }
+    if (count == 2)
+        return (1);
+    return (0);
+}
 
 int     ft_find_next_prime(int nb)
 {
+    int     i;
 
-}
-
-int     main(void)
-{
+    i = nb;
+    while (i != ft_check_if_prime(i))
+    {
+        if (ft_check_if_prime(i))
+            return (i);
+        i++;
+    }
     return (0);
 }
