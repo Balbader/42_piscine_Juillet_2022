c50444385ed95da2c679657a47ce37753d1c1ea1
balou <baloucode83@gmail.com>
2022-05-12 10:38:59 +0200

    ft_fibonacci.c

--- forty_two_prep/C05/ex04/ft_fibonacci.c
+++ forty_two_prep/C05/ex04/ft_fibonacci.c
@@ -2,10 +2,13 @@
 
 int     ft_fibonacci(int index)
 {
-
+    if (index < 0)
+        return (-1);
+    return (0);
 }
 
 int     main(void)
 {
+    printf("%d\n", ft_fibonacci(-5));
     return (0);
 }
