c464a0c6f7ca7297bbad8e3d749c86b56839dd16
balou <baloucode83@gmail.com>
2022-05-13 20:47:56 +0200

    ft_print_program_name.c

--- forty_two_prep/C06/ex00/ft_print_program_name.c
+++ forty_two_prep/C06/ex00/ft_print_program_name.c
@@ -13,9 +13,14 @@ void    ft_putstr(char *str)
     write(1, "\n", 1);
 }
 
+void    ft_print_program_name(char *name)
+{
+    ft_putstr(name);
+}
+
 int     main(int argc, char *argv[])
 {
     (void) argc;
-    ft_putstr(argv[0]);
+    ft_print_program_name(argv[0]);
     return (0);
 }
