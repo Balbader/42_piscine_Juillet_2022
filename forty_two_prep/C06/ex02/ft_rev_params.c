e69edd6efa316751d1b194c606266fcb5abaff87
balou <baloucode83@gmail.com>
2022-05-14 17:32:22 +0200

    ft_rev_params.c

--- forty_two_prep/C06/ex02/ft_rev_params.c
+++ forty_two_prep/C06/ex02/ft_rev_params.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void    ft_putstr(char *str)
 {
@@ -12,7 +13,24 @@ void    ft_putstr(char *str)
     }
 }
 
+void    ft_print_rev_params(int nb, char **str)
+{
+    char    *params_reversed[200];
+    int     i;
+
+    i = 0;
+    while (nb > 0)
+    {
+        params_reversed[i] = str[nb];
+        printf("%s\n", str[nb]);
+        printf("%s\n", params_reversed[i]);
+        nb--;
+        i++;
+    }
+}
+
 int     main(int argc, char **argv)
 {
+    ft_print_rev_params(argc, argv);
     return (0);
 }
