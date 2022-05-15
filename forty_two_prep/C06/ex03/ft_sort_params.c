7a3f8106c13d08b89a8a59433a86518f9ca99258
balou <baloucode83@gmail.com>
2022-05-15 21:09:40 +0200

    ft_sort_params.c

--- forty_two_prep/C06/ex03/ft_sort_params.c
+++ forty_two_prep/C06/ex03/ft_sort_params.c
@@ -14,12 +14,26 @@ void    ft_putstr(char *str)
 
 void    ft_sort_params(int nb, char **str)
 {
+    char    *temp[250];
     int     i;
 
+    temp[0] = str[1];
+    ft_putstr("Before: ");
+    ft_putstr(*temp);
+    write(1, "\n", 1);
     i = 1;
+    while (i < nb)
+    {
+        if (temp[0][0] > str[i][0])
+            temp[0] = str[i];
+        i++;
+    }
+    ft_putstr("After: ");
+    ft_putstr(*temp);
 }
 
 int     main(int argc, char **argv)
 {
+    ft_sort_params(argc, argv);
     return (0);
 }
