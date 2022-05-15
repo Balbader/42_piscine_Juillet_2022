e217e14cc12c0230a55a1470150370f86a0df33b
balou <baloucode83@gmail.com>
2022-05-15 19:45:23 +0200

    ft_sort_params.c

---
+++ forty_two_prep/C06/ex03/ft_sort_params.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+
+void    ft_putstr(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+    {
+        write(1, &str[i], 1);
+        i++;
+    }
+}
+
+void    ft_sort_params(int nb, char **str)
+{
+    int     i;
+
+    i = 1;
+}
+
+int     main(int argc, char **argv)
+{
+    return (0);
+}
