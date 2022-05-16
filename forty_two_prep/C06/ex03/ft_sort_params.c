b4f0c89d6d52eb1d167aaea60e341a6214517fd4
balou <baloucode83@gmail.com>
2022-05-16 07:32:13 +0200

    ft_sort_params.c

--- forty_two_prep/C06/ex03/ft_sort_params.c
+++ forty_two_prep/C06/ex03/ft_sort_params.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 void    ft_putstr(char *str)
 {
@@ -12,24 +13,48 @@ void    ft_putstr(char *str)
     }
 }
 
-void    ft_sort_params(int nb, char **str)
+char    ft_strcpy(char *dest, char *src)
 {
-    char    *temp[250];
     int     i;
 
-    temp[0] = str[1];
-    ft_putstr("Before: ");
-    ft_putstr(*temp);
-    write(1, "\n", 1);
-    i = 1;
-    while (i < nb)
+    i = 0;
+    while (src[i])
     {
-        if (temp[0][0] > str[i][0])
-            temp[0] = str[i];
+        dest[i] = src[i];
         i++;
     }
-    ft_putstr("After: ");
-    ft_putstr(*temp);
+    dest[i] = '\0';
+    return (*dest);
+}
+
+int     ft_strcmp(char *s1, char *s2)
+{
+    return ((unsigned int)*s1 - (unsigned int)*s2);
+}
+
+void    ft_sort_params(int nb, char **str)
+{
+    char    temp[250];
+    int     i;
+    int     j;
+
+    i = 1;
+    j = i + 1;
+    while (i < nb)
+    {
+        while (j < nb)
+        {
+            if(ft_strcmp(str[i], str[j]) > 0)
+            {
+                ft_strcpy(temp, str[i]);
+                ft_strcpy(str[i], str[j]);
+                ft_strcpy(str[j], temp);
+                printf("%s\n", str[i]);
+            }
+            j++;
+        }
+        i++;
+    }
 }
 
 int     main(int argc, char **argv)
