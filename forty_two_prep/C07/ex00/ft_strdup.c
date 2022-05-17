b73987c614612d17fb0a27700679667008b65e42
balou <baloucode83@gmail.com>
2022-05-17 11:59:09 +0200

    ft_strdup.c

--- forty_two_prep/C07/ex00/ft_strdup.c
+++ forty_two_prep/C07/ex00/ft_strdup.c
@@ -1,7 +1,23 @@
 #include <stdlib.h>
 
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
 char    *ft_strdup(char *src)
 {
+    char    *duplicate;
+    int     src_len;
+
+    src_len = ft_strlen(src);
+    duplicate = (char *)malloc(src_len * sizeof(char));
+
     return (0);
 }
 
