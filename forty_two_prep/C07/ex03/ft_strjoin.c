9baf16e22a933fc31502db72be0021de5198f98f
balou <baloucode83@gmail.com>
2022-05-17 15:05:06 +0200

    ft_strjoin.c

---
+++ forty_two_prep/C07/ex03/ft_strjoin.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char    *ft_strjoin(int size, char **strs, char *sep)
+{
+    char    *final_str;
+
+    if (size == 0)
+    {
+        *strs = NULL;
+        free(strs);
+    }
+    return (final_str);
+}
+
+int     main(void)
+{
+    return (0);
+}
