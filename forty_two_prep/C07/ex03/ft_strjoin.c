4595bfe06545b78ea2330e24a80c71d42436a684
balou <baloucode83@gmail.com>
2022-05-18 14:25:36 +0200

    ft_strjoin.c

--- forty_two_prep/C07/ex03/ft_strjoin.c
+++ forty_two_prep/C07/ex03/ft_strjoin.c
@@ -1,15 +1,31 @@
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 
-void    ft_putstr(char *str)
+int     ft_strlen(char *str)
 {
     int     i;
 
     i = 0;
     while (str[i])
         i++;
-    write(1, str, i);
+    return (i);
+}
+
+int     final_size(int size, char **strs, char *sep)
+{
+    int     i;
+    int     tot_len;
+
+    i = 0;
+    tot_len = 0;
+    while (i < size)
+    {
+        tot_len = tot_len + ft_strlen(strs[i]);
+        i++;
+    }
+    if (!size)
+        return (1);
+    tot_len = tot_len + ft_strlen(sep) * (size - 1) + 1;
+    return (tot_len);
 }
 
 char    *ft_strcat(char *dest, char *src)
@@ -37,28 +53,16 @@ char    *ft_strjoin(int size, char **strs, char *sep)
     int     len;
     int     i;
 
-    len = size * 2;
-    final_str = (char *)malloc(len * sizeof(char *));
-    if (size == 0)
-    {
-        *strs = NULL;
-        free(strs);
-    }
+    len = final_size(size, strs, sep);
+    if (!(final_str = (char *)malloc(len * sizeof(char))))
+        return (0);
     i = 0;
-    while (i < len)
+    while (i < size)
     {
         ft_strcat(final_str, strs[i]);
-        if (i < len - 1)
+        if (i < size - 1)
             ft_strcat(final_str, sep);
         i++;
     }
     return (final_str);
-}
-
-int     main(void)
-{
-    char    *strs[] = {"Hello", "my", "name", "is", "balou"};
-    char    *sep = "+";
-    printf("%s\n", ft_strjoin(6, strs, sep));
-    return (0);
 }
