bfbc660d816ebac467f19528063fd2df34033795
balou <baloucode83@gmail.com>
2022-05-20 14:58:09 +0200

    ft_atoi_base.c

--- forty_two_prep/C04/ex05/ft_atoi_base.c
+++ forty_two_prep/C04/ex05/ft_atoi_base.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+
+int     ft_strlen(char *str)
+{
+    int     i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
 int     ft_atoi_base(char *str, char *base)
 {
 
