a91b5d5eb3581e23a03ed94453df611f819d5dc5
balou <baloucode83@gmail.com>
2022-05-22 21:44:21 +0200

    ft_atoi_base.c

--- forty_two_prep/C04/ex05/ft_atoi_base.c
+++ forty_two_prep/C04/ex05/ft_atoi_base.c
@@ -10,6 +10,22 @@ int     ft_strlen(char *str)
     return (i);
 }
 
+int     ft_check_base(char *base)
+{
+    int     i;
+
+    i = 0;
+    while (base[i])
+    {
+        if (base[i] == '+' || base[i] == '-' || base[i] == '\t'
+                || base[i] == '\n' || base[i] == '\r'
+                || base[i] == '\v' || base[i] == ' ')
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
 int     ft_atoi_base(char *str, char *base)
 {
 
