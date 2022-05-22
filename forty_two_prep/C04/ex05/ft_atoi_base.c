6542807f1e853787475837d1da97e98a851e2d79
balou <baloucode83@gmail.com>
2022-05-22 22:19:26 +0200

    ft_atoi_base.c

--- forty_two_prep/C04/ex05/ft_atoi_base.c
+++ forty_two_prep/C04/ex05/ft_atoi_base.c
@@ -10,17 +10,52 @@ int     ft_strlen(char *str)
     return (i);
 }
 
-int     ft_check_base(char *base)
+int     ft_check_base(char c)
+{
+    if (c == '+' || c == '-' || c == ' ' || c == '\t' || c == '\n' || c == '\r'
+            || c == '\v')
+        return (0);
+    return (1);
+}
+
+int     ft_base_position(char *base, char c)
 {
     int     i;
 
     i = 0;
+    while (i < ft_strlen(base))
+    {
+        if (base[i] == c)
+            return (i);
+        i++;
+    }
+    return (-1);
+}
+
+int     ft_base_is_ok(char *base)
+{
+    unsigned int    i;
+    unsigned int    j;
+
+    i = 0;
+    if (ft_strlen(base) < 2)
+        return (0);
     while (base[i])
     {
-        if (base[i] == '+' || base[i] == '-' || base[i] == '\t'
-                || base[i] == '\n' || base[i] == '\r'
-                || base[i] == '\v' || base[i] == ' ')
+        if (ft_check_base(base[i]))
             return (0);
+        i++;
+    }
+    i = 0;
+    while (base[i])
+    {
+        j = i + 1;
+        while (base[j])
+        {
+            if (base[i] == base[j])
+                return (0);
+            j++;
+        }
         i++;
     }
     return (1);
