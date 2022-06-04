f4c0f3e5b262898944b71e9cc6d0db37a5d95f00
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-04 18:49:27 +0200

    test.c

--- forty_two_prep/C07/ex05/test.c
+++ forty_two_prep/C07/ex05/test.c
@@ -1,43 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char    *ft_strcpy(char *dest, char *src)
-{
-    int     i;
-
-    i = 0;
-    while (src[i])
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
-}
-
 int     main(void)
 {
-    char    str[] = "Hello world my name is balou";
-    //char    res[250][250];
-    char    temp[250];
+    char    *str = "Hello my name is balou";
+    char    res[50][50];
     int     i;
     int     j;
+    int     k;
 
     i = 0;
-    while(str[i])
-    {
-        if (str[i] != ' ')
-            temp[i] = str[i];
-        else
-            temp[i] = '\0';
-        i++;
-    }
-    printf("%s\n", temp);
     j = 0;
-    while (str[j])
+    k = 0;
+    while (str[i])
     {
-        printf("%c", str[j]);
+        while (str[i] != ' ')
+        {
+            res[j][k] = str[i];
+            k++;
+            i++;
+        }
+        printf("res[%d]: %s\n", j, res[j]);
+        k = 0;
         j++;
+        i++;
     }
     return (0);
 }
