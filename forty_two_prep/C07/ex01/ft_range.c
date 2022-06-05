669495b6f5914e57aa880375fc4fe788806f5b5e
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-05 15:18:34 +0200

    ft_range.c

--- forty_two_prep/C07/ex01/ft_range.c
+++ forty_two_prep/C07/ex01/ft_range.c
@@ -3,9 +3,28 @@
 
 int *ft_range(int min, int max)
 {
+	int *range;
+	int i;
+
+	if (min >= max)
+		range = NULL;
+	range = (int *)malloc(sizeof(int) * (max - min) - 1);
+	i = 0;
+	while (min < max)
+	{
+		range[i] = min;
+		min++;
+		i++;
+	}
+	return (range);
 }
 
 int main(void)
 {
+	int *test;
+
+	test = ft_range(0, 10);
+	for (int i = 0; i < 10; i++)
+		printf("%d\n", test[i]);
 	return (0);
 }
