64e964c74b68e69129c72d894321a7e72ee820db
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-05 15:54:18 +0200

    ft_ultimate_range.c

--- forty_two_prep/C07/ex02/ft_ultimate_range.c
+++ forty_two_prep/C07/ex02/ft_ultimate_range.c
@@ -3,9 +3,21 @@
 
 int ft_ultimate_range(int **range, int min, int max)
 {
-}
+	int i;
 
-int main(void)
-{
-	return (0);
+	if (min >= max)
+	{
+		*range = NULL;
+		return (0);
+	}
+	if (!(*range = (int *)malloc(sizeof(int) * (max - min) - 1)))
+		return (-1);
+	i = 0;
+	while (i < (max - min))
+	{
+		*range[i] = min;
+		min++;
+		i++;
+	}
+	return (max - min);
 }
