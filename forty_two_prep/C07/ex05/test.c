fc6849c4c21470460a3000d467aa30d6687471a2
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-05 13:09:30 +0200

    text.c

--- forty_two_prep/C07/ex05/test.c
+++ forty_two_prep/C07/ex05/test.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int ft_count_str_elem(char *str, char *sep)
+{
+    int i;
+    int count;
+
+    count = 0;
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] == sep)
+            count++;
+        i++;
+    }
+    return (count + 1);
+}
+
 int main(void)
 {
     char *str = "Hello my name is balou";
     char res[50][50];
+    int count;
     int i;
     int j;
     int k;
@@ -19,11 +36,16 @@ int main(void)
         i++;
         if (str[i] == ' ')
         {
-            printf("res[%d]: %s\n", j, res[j]);
             k = 0;
             j++;
             i++;
         }
+    }
+    i = 0;
+    while (i < 50)
+    {
+        printf("res[%d]: %s\n", i, res[i]);
+        i++;
     }
     return (0);
 }
