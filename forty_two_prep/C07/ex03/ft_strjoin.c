5cb3039bac6b041f02e4b98c13720c6427ed8bdf
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-05 21:13:53 +0200

    ft_strjoin.c

--- forty_two_prep/C07/ex03/ft_strjoin.c
+++ forty_two_prep/C07/ex03/ft_strjoin.c
@@ -1,6 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
 char *ft_strcat(char *dest, char *src)
 {
 	int i;
