05130f413e614a28c0e5d5f8510ff192979299dc
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-06 22:13:37 +0200

    ft_split.c

--- forty_two_prep/C07/ex05/ft_split.c
+++ forty_two_prep/C07/ex05/ft_split.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//  1. create an 2d array that will take in every segment of the string the is
-//     preceded and followed by any character within the *split string.
+// The Idea:
+// Create an 2d array that will take in every segment of the string the is
+// preceded and followed by any character within the *split string.
+
+// Steps:
+// 1. Run through the main string and save to temp every character encountered
+// 2. As soon as you encounter a seperator character,
+//  	all characters saved in temp will constitute the string to be saved
+// 3. Reset temp to 0
+// 4. Start running through the main string from
+//    the last seperator character encountered
+// 5. Repeat 1 => 4 till there is no more seperator characters encountered
 
 char **ft_split(char *str, char *charset)
 {
