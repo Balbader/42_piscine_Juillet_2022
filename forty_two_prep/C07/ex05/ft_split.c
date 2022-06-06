8c7476ac98baa4fe2baa2a36d7fbbd2baad7c894
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-06 21:46:38 +0200

    ft_split.c

--- forty_two_prep/C07/ex05/ft_split.c
+++ forty_two_prep/C07/ex05/ft_split.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//  1. create an 2d array that will take in every segment of the string the is
+//     preceded and followed by any character within the *split string.
+
 char **ft_split(char *str, char *charset)
 {
 	char **tab;
@@ -10,6 +13,6 @@ char **ft_split(char *str, char *charset)
 int main(void)
 {
 	char *str = "Hello my name is balou";
-	char *split = " ";
+	char *split = "aeiou";
 	return (0);
 }
