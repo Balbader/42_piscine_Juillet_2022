1079d5b65ca1b3ad8f8ab4fda3a52c42c816beca
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-06 17:38:41 +0200

    ft_split.c

---
+++ forty_two_prep/C07/ex05/ft_split.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+char **ft_split(char *str, char *charset)
+{
+	char **tab;
+	return (tab);
+}
+
+int main(void)
+{
+	char *str = "Hello my name is balou";
+	char *split = "SPLIT";
+	return (0);
+}
