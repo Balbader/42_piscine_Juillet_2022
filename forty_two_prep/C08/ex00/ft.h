a6464ed6a24761827b5bc3a7455d2ec73a5a28a3
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-10 18:39:30 +0200

    ft.h

--- forty_two_prep/C08/ex00/ft.h
+++ forty_two_prep/C08/ex00/ft.h
@@ -1,5 +1,5 @@
-#ifndef FT_H // IF NOT DEFINED
-#define FT_H // DEFINE
+#ifndef FT_H
+#define FT_H
 
 void ft_putchar(char c);
 void ft_swap(int *a, int *b);
@@ -7,4 +7,4 @@ void ft_putstr(char *str);
 int ft_strlen(char *str);
 int ft_strcmp(char *s1, char *s2);
 
-#endif // END OF DEFINITION
+#endif
