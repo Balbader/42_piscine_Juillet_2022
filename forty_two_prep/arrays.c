d96670a6019125b82a058121344e2a2d8b7c1c04
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-12 19:05:21 +0200

    arrays.c

--- forty_two_prep/arrays.c
+++ forty_two_prep/arrays.c
@@ -2,5 +2,17 @@
 
 int main(void)
 {
+	char *tab[] = {"Hello", "My", "Name", "Is", "Balou"};
+	char *str = "aeiou";
+	int size;
+	int i;
+
+	size = sizeof(tab) / sizeof(tab[0]);
+	i = 0;
+	while (i < size)
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
 	return (0);
 }
