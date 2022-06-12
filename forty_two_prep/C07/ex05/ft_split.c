e073d993cebf17f4d32372a1117d7e2e196c154c
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-12 07:21:10 +0200

    ft_split.c

--- forty_two_prep/C07/ex05/ft_split.c
+++ forty_two_prep/C07/ex05/ft_split.c
@@ -1,47 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// The Idea:
-// Create an 2d array that will take in every segment of the string the is
-// preceded and followed by any character within the *split string.
-
-// Steps:
-// 1. Run through the main string and COPY to temp every character encountered
-// 2. As soon as you encounter a seperator character,
-//  	all characters saved in temp will constitute the string to be saved
-// 3. Reset temp to 0
-// 4. Start running through the main string from
-//    the last seperator character encountered
-// 5. Repeat 1 => 4 till there is no more seperator characters encountered
-
-int ft_is_sep(char *split)
-{
-}
-
-char *ft_strcpy(char *dest, char *src)
+/* Compare string character and seperator */
+int ft_is_sep(char c, char *split)
 {
 	int i;
 
 	i = 0;
-	while (src[i])
+	while (split[i])
 	{
-		dest[i] = src[i];
+		if (split[i] == c)
+			return (1);
 		i++;
 	}
-	dest[i] = '\0';
-	return (dest);
+	return (0);
+}
+
+/* Count the total number of occurences of seperator in string */
+int ft_count_split(char *str, char *split)
+{
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	while (str[i])
+	{
+		while (str[i] && ft_is_sep(str[i], split) == 1)
+			i++;
+		while (str[i] && ft_is_sep(str[i], split) == 0)
+			i++;
+		count++;
+	}
+	if (ft_is_sep(str[i - 1], split) == 1)
+		count--;
+	return (count);
 }
 
 char **ft_split(char *str, char *charset)
 {
 	char **tab;
+	int i;
+	int j;
+	int k;
+
+	/* If not enough space return 0 */
+	if (!(tab = malloc(sizeof(char *) * (ft_count_split(str, charset) + 1))))
+		return (0);
+
+	i = 0;
+	k = 0;
+	while (k < ft_count_split(str, charset))
+	{
+		while (str[i] && ft_is_sep(str[i], charset) == 1)
+			i++;
+		j = i;
+		while (str[j] && ft_is_sep(str[j], charset) == 0)
+			j++;
+
+		/* If not enough space return 0 */
+		if (!(tab[k] = malloc(sizeof(char) * j)))
+			return (0);
+
+		j = 0;
+		while (ft_is_sep(str[i], charset) == 0 && str[i])
+			tab[k][j++] = str[i++];
+		tab[k++][j] = 0;
+	}
+	tab[k] = 0;
 	return (tab);
 }
 
 int main(void)
 {
-	const char *str = "Hello my name is balou";
-	const char *split = "aeiou";
+	char *str = "Hello my name is balou";
+	char *split = " aeiou";
+	char **test;
+	int i;
 
+	i = 0;
+	test = ft_split(str, split);
+	while (test[i])
+	{
+		printf("%s\n", test[i]);
+		i++;
+	}
 	return (0);
 }
