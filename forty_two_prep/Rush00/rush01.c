21242c57dc73d39acf8ef78bfed2e2310ec48cb5
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-17 17:18:20 +0200

    rush01.c

--- forty_two_prep/Rush00/rush01.c
+++ forty_two_prep/Rush00/rush01.c
@@ -2,9 +2,25 @@
 
 void ft_putchar(char c);
 
-/*
-1. When i = 0
-*/
+void ft_printf_first_line(int x, int i)
+{
+	if (i == 0)
+		ft_putchar('/');
+	else if (i == x - 1)
+		ft_putchar('\\');
+	else if (i > 0 && i < x - 1)
+		ft_putchar('*');
+}
+
+void ft_printf_last_line(int x, int i)
+{
+	if (i == 0)
+		ft_putchar('\\');
+	else if (i == x - 1)
+		ft_putchar('/');
+	else if (i > 0 && i < x - 1)
+		ft_putchar('*');
+}
 
 void ft_print_line(int x, int y)
 {
@@ -18,23 +34,9 @@ void ft_print_line(int x, int y)
 		while (i < x)
 		{
 			if (j == 0)
-			{
-				if (i == 0)
-					ft_putchar('/');
-				else if (i == x - 1)
-					ft_putchar('\\');
-				else if (i > 0 && i < x - 1)
-					ft_putchar('*');
-			}
+				ft_printf_first_line(x, i);
 			else if (j == y - 1)
-			{
-				if (i == 0)
-					ft_putchar('\\');
-				else if (i == x - 1)
-					ft_putchar('/');
-				else if (i > 0 && i < x - 1)
-					ft_putchar('*');
-			}
+				ft_printf_last_line(x, i);
 			else if (i == 0 || i == x - 1)
 				ft_putchar('*');
 			else if (i > 0 && i < x - 1)
@@ -52,7 +54,7 @@ void rush(int x, int y)
 		return;
 	else if (x == 1 && y == 1)
 	{
-		ft_putchar('o');
+		ft_putchar('/');
 		ft_putchar('\n');
 	}
 	else
