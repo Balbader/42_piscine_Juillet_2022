49c140ca5e5c0a7941870afc746d31bab164b7f4
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-20 17:12:10 +0200

    ft_is_sort.c

--- forty_two_prep/C11/ex04/ft_is_sort.c
+++ forty_two_prep/C11/ex04/ft_is_sort.c
@@ -1,11 +1,28 @@
-int     ft_is_sort(int *tab, int length, int (*f)(int, int))
+int ft_determine_sign(int i, int *tab, int (*f)(int, int))
 {
-    int     i;
+    if ((*f)(tab[i - 1], tab[i]) < 0)
+        return (-1);
+    if ((*f)(tab[i - 1], tab[i]) > 0)
+        return (1);
+    return (0);
+}
 
-    i = 0;
+int ft_is_sort(int *tab, int length, int (*f)(int, int))
+{
+    int i;
+    int sign;
+    int cmp;
+
+    i = 1;
+    sign = 0;
+    while (i < length && !(*f)(tab[i - 1], tab[i]))
+        i++;
+    if (!sign && i < length)
+        sign = ft_determine_sign(i++, tab, (f));
     while (i < length)
     {
-        if ((*f)(tab[i], tab[i + 1]) >= 0)
+        cmp = ft_determine_sign(i, tab, (f));
+        if (sign != cmp && cmp)
             return (0);
         i++;
     }
