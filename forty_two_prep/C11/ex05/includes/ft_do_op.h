6fdeb28a85a478c86bbf19d0bc65512bf147a40c
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-20 18:18:56 +0200

    ft_do_op.h

---
+++ forty_two_prep/C11/ex05/includes/ft_do_op.h
@@ -0,0 +1,16 @@
+#ifndef FT_DO_OP_H
+#define FT_DO_OP_H
+#include <unistd.h>
+
+void    ft_putnbr(int nbr);
+void    ft_do_op(int a, int b, char *ope);
+int     ft_atoi(char *str);
+int     ft_is_op(char *str);
+
+void    ft_add(int a, int b);
+void    ft_sub(int a, int b);
+void    ft_div(int a, int b);
+void    ft_mod(int a, int b);
+void    ft_mult(int a, int b);
+
+#endif
