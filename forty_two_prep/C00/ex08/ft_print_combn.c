4212ec48ed21b8e125f7f7de83fa4246412c1065
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-23 08:10:53 +0200

    ft_print_combn.c

--- forty_two_prep/C00/ex08/ft_print_combn.c
+++ forty_two_prep/C00/ex08/ft_print_combn.c
@@ -1,10 +1,5 @@
 #include <unistd.h>
 
-void    ft_putchar(char c)
-{
-    write(1, &c, 1);
-}
-
 void	ft_init_arr(int *tab, int n)
 {
     int i;
@@ -64,34 +59,4 @@ void	ft_print_combn(int n)
         }
         i = 0;
     }
-}
-
-int     main(void)
-{
-    ft_print_combn(1);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(2);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(3);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(4);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(5);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(6);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(7);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(8);
-    ft_putchar('\n');
-    ft_putchar('\n');
-    ft_print_combn(9);
-    return (0);
 }
