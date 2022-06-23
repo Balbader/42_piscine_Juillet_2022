fc3651a9beddf0bd4563c7e1617ba9cc9b129513
Balbader <15971200+Balbader@users.noreply.github.com>
2022-06-23 06:57:47 +0200

    ft_advanced_sort_string_tab.c

---
+++ forty_two_prep/C11/ex07/ft_advanced_sort_string_tab.c
@@ -0,0 +1,29 @@
+void	ft_swap(char **s1, char **s2)
+{
+	char	*temp;
+
+	temp = *s1;
+	*s1 = *s2;
+	*s2 = temp;
+}
+
+void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *))
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (!tab)
+		return ;
+	while (tab[i])
+	{
+		j = 0;
+		while (tab[j + 1])
+		{
+			if ((*cmp)(tab[j], tab[j + 1]) > 0)
+				ft_swap(&tab[j], &tab[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
