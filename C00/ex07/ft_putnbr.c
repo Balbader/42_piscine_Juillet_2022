e8514369990318753a4d9045fa34108c2bb909cf
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 11:55:33 +0200

    ft_putnbr.c

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 11:48:10 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 11:55:14 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
@@ -10,18 +22,18 @@ void	ft_putnbr(int nb)
 	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		return;
+		return ;
 	}
-	if (nb < 0)
+	if (nb >= -2147483647 && nb < 0)
 	{
 		ft_putchar('-');
 		nb *= -1;
 	}
-	if (nb > 9)
+	if (nb < 10)
+		ft_putchar(nb + '0');
+	else
 	{
 		ft_putnbr(nb / 10);
-		ft_putchar(nb % 10 + '0');
+		ft_putnbr(nb % 10);
 	}
-	else
-		ft_putchar(nb + '0');
 }
