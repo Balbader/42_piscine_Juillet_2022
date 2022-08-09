072ab1ee85d03a5b0a46a682c55db22964c9b162
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 14:12:56 +0200

    ft_sort_int_tab.c

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,23 +1,30 @@
-void    ft_sort_int_tab(int *tab, int size)
-{
-    int     temp;
-    int     i;
-    int     j;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 13:54:19 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 14:12:43 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (i <= size)
-    {
-        j = i + 1;
-        while (j <= size)
-        {
-            if (tab[i] > tab[j])
-            {
-                temp = tab[i];
-                tab[i] = tab[j];
-                tab[j] = temp;
-            }
-            j++;
-        }
-        i++;
-    }
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	temp;
+
+	temp = 0;
+	i = 0;
+	while (i < size)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			temp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = temp;
+		}
+		i++;
+	}
 }
