14a476f28368e9742120e294e8ad45d59893ee3c
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 17:46:46 +0200

    ft_strlowcase.c

--- C02/ex08/ft_strlowcase.c
+++ C02/ex08/ft_strlowcase.c
@@ -1,13 +1,25 @@
-char    *ft_strlowcase(char *str)
-{
-    int     i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 17:46:19 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 17:46:33 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] -= 32;
-        i++;
-    }
-    return (str);
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
 }
