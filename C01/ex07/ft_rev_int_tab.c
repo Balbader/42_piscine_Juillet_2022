66e0f81dc4158fff8fe267eb55dd8e232b222805
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 13:53:33 +0200

    ft_rev_int_tab.c

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -1,16 +1,32 @@
-void    ft_rev_int_tab(int *tab, int size)
-{
-    int     i;
-    int     temp;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 13:31:55 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 13:53:23 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    size -= 1;
-    while (i < size)
-    {
-        temp = tab[i];
-        tab[i] = tab[size];
-        tab[size] = temp;
-        i++;
-        size--;
-    }
+#include <stdio.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	s;
+	int	i;
+
+	s = size - 1;
+	temp = 0;
+	i = 0;
+	while (i < size / 2)
+	{
+		temp = tab[i];
+		tab[i] = tab[s];
+		tab[s] = temp;
+		i++;
+		s--;
+	}
 }
