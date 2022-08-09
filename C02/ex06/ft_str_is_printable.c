cd98a4106638644d7f7054669ac5854574f80691
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 17:45:02 +0200

    ft_strupcase.c

--- C02/ex06/ft_str_is_printable.c
+++ C02/ex06/ft_str_is_printable.c
@@ -1,19 +1,34 @@
-int     ft_str_is_printable(char *str)
-{
-    int     i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 17:38:33 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 17:39:20 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    if (str[i] == '\0')
-        return (1);
-    else
-    {
-        while (str[i] != '\0')
-        {
-            if (!(str[i] >= 32 && str[i] <= 126))
-                return (0);
-            else
-                i++;
-        }
-    }
-    return (1);
+int	ft_is_printable(char c)
+{
+	if (!(c >= 32 && c <= 126))
+		return (1);
+	return (0);
+}
+
+int	ft_str_is_printable(char *str)
+{
+	int	i;
+
+	if (str[i] == '\0')
+		return (1);
+	i = 0;
+	while (str[i])
+	{
+		if (ft_is_printable(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
--- C02/ex07/ft_strupcase.c
+++ C02/ex07/ft_strupcase.c
@@ -1,13 +1,25 @@
-char    *ft_strupcase(char *str)
-{
-    int     i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 17:40:23 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 17:44:25 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 'a' && str[i] <= 'z')
-            str[i] -= 32;
-        i++;
-    }
-    return (str);
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
 }
