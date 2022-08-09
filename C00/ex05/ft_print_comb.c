3cc34efb74883a9edcfd20790f9481ba3eff86d6
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 11:33:32 +0200

    ft_print_comb.c

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -1,29 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 11:24:40 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 11:33:07 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_print_comb(void)
+void	ft_putchar(char c)
 {
-    int     a;
-    int     b;
-    int     c;
+	write(1, &c, 1);
+}
 
-    a = '0';
-    while (a <= '7')
-    {
-        b = a + 1;
-        while (b <= '8')
-        {
-            c = b + 1;
-            while (c <= '9')
-            {
-                write(1, &a, 1);
-                write(1, &b, 1);
-                write(1, &c, 1);
-                if (a != '7')
-                    write(1, ", ", 2);
-                c++;
-            }
-            b++;
-        }
-        a++;
-    }
+void	ft_print_char(int a, int b, int c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print_char(a, b, c);
+				if (a != '7')
+					write(1, ", ", 2);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
 }
