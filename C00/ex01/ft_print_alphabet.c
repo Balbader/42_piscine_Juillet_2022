9a2ad56d67705159373b044572d36f3fb31f95bc
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 11:13:04 +0200

    ft_print_alphabet.c

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 11:10:37 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 11:12:43 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_print_alphabet(void)
+void	ft_print_alphabet(void)
 {
-    write(1, "abcdefghifklmnopqrstuvwxyz", 26);
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
 }
