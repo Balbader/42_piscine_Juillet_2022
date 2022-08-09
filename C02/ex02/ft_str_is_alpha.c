21e997b22bda61178b22cfd963d11075a68b37b4
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 17:29:47 +0200

    ft_str_is_alpha.c

--- C02/ex02/ft_str_is_alpha.c
+++ C02/ex02/ft_str_is_alpha.c
@@ -1,20 +1,32 @@
-int     ft_str_is_alpha(char *str)
-{
-    int     i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 17:17:14 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 17:29:22 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    if (str[i] == '\0')
-        return (1);
-    else
-    {
-        while (str[i] != '\0')
-        {
-            if (!((str[i] >= 'a' && str[i] <= 'z') ||
-                    (str[i] >= 'A' && str[i] <= 'Z')))
-                return (0);
-            else
-                i++;
-        }
-    }
-    return (1);
+int	ft_is_alpha(char c)
+{
+	if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')))
+		return (1);
+	return (0);
+}
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_is_alpha(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
