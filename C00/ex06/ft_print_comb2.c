ab82dd2b459acdc6b1dcd5d962badb8cb9a34ca4
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 11:47:45 +0200

    ft_print_comb2.c

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -1,35 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 11:34:01 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 11:47:26 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-  write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void    ft_print_numbers(int a, int b)
+void	ft_print_chars(int a, int b)
 {
-  ft_putchar(a / 10 + '0');
-  ft_putchar(a % 10 + '0');
-  ft_putchar(' ');
-  ft_putchar(b / 10 + '0');
-  ft_putchar(b % 10 + '0');
+	ft_putchar(a / 10 + '0');
+	ft_putchar(a % 10 + '0');
+	ft_putchar(' ');
+	ft_putchar(b / 10 + '0');
+	ft_putchar(b % 10 + '0');
 }
 
-void    ft_print_comb2(void)
+void	ft_print_comb2(void)
 {
-  int   a;
-  int   b;
+	int	a;
+	int	b;
 
-  a = 0;
-  while (a <= 98)
-  {
-    b = a + 1;
-    while (b <= 99)
-    {
-      ft_print_numbers(a, b);
-      if (a != 98 || b != 99)
-        write(1, ", ", 2);
-      b++;
-    }
-    a++;
-  }
+	a = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_print_chars(a, b);
+			if (a != 98)
+				write(1, ", ", 2);
+			b++;
+		}
+		a++;
+	}
 }
