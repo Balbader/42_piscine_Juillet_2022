0daeaea847f5a71c9302d028a2b8e82d688ec383
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 17:31:52 +0200

    ft_str_is_numeric.c

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -1,19 +1,32 @@
-int     ft_str_is_numeric(char *str)
-{
-    int     i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 17:30:50 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 17:31:35 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    if (str[i] == '\0')
-        return (1);
-    else
-    {
-        while (str[i] != '\0')
-        {
-            if (!(str[i] >= '0' && str[i] <= '9'))
-                return (0);
-            else
-                i++;
-        }
-    }
-    return (1);
+int	ft_is_nbr(char c)
+{
+	if (!(c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (ft_is_nbr(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
