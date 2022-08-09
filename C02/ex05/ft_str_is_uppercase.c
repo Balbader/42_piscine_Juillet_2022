f5cd07d93a74645e4592230ccde11416359b6fd1
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-09 17:37:42 +0200

    ft_str_is_uppercase.c

--- C02/ex05/ft_str_is_uppercase.c
+++ C02/ex05/ft_str_is_uppercase.c
@@ -1,19 +1,34 @@
-int     ft_str_is_uppercase(char *str)
-{
-    int     i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_uppercase.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/09 17:32:56 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/09 17:37:32 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    if (str[i] == '\0')
-        return (1);
-    else
-    {
-        while (str[i] != '\0')
-        {
-            if (!(str[i] >= 'A' && str[i] <= 'Z'))
-                return (0);
-            else
-                i++;
-        }
-    }
-    return (1);
+int	ft_is_lower(char c)
+{
+	if (!(c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	if (str[i] == '\0')
+		return (1);
+	i = 0;
+	while (str[i])
+	{
+		if (ft_is_lower(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
