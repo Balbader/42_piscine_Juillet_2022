502512c756b004d06e70969b28d6dcc78035f7cb
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-10 08:42:19 +0200

    ft_strcapitalize.c

--- C02/ex09/ft_strcapitalize.c
+++ C02/ex09/ft_strcapitalize.c
@@ -6,11 +6,39 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/10 08:24:56 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/10 08:28:39 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/10 08:34:57 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* turn str to lowercase */
+char	*ft_to_lower(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+/*
 char	*ft_strcapitalize(char *str)
 {
 	return (str);
+}
+*/
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	str[] = "            saLut, coMMent tu vAs ? 42Mots quarANTe-deux; cinquaNte+Et+uN         ";
+
+	ft_to_lower(str);
+	printf("%s\n", str);
+	return (0);
 }
