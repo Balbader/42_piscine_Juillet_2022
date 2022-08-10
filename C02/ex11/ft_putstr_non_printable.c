f2f7c6f812ae8632c704328fd892af0c1ccfa5ef
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-10 16:34:56 +0200

    ft_putstr_non_printble.c

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -1,34 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 16:30:00 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/10 16:34:27 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void    ft_make_hex_printable(int nb)
+void	ft_make_hex_printable(int nb)
 {
-    char    hex[] = "0123456789abcdef";
+	char	*hex;
 
-    ft_putchar(hex[nb / 16]);
-    ft_putchar(hex[nb % 16]);
+	hex = "0123456789abcdef";
+	ft_putchar(hex[nb / 16]);
+	ft_putchar(hex[nb % 16]);
 }
 
-void    ft_putstr_non_printable(char *str)
+void	ft_putstr_non_printable(char *str)
 {
-    int     i;
-    char    c;
+	int	i;
+	int	c;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 32 && str[i] <= 126)
-            ft_putchar(str[i]);
-        else
-        {
-            c = '\\';
-            ft_putchar(c);
-            ft_make_hex_printable(str[i]);
-        }
-        i++;
-    }
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+			ft_putchar(str[i]);
+		else
+		{
+			c = '\\';
+			ft_putchar(c);
+			ft_make_hex_printable(str[i]);
+		}
+		i++;
+	}
 }
