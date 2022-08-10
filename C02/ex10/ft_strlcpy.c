59f868889a22432f9626609ce72bff94632c453c
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-10 15:56:43 +0200

    ft_strlcpy.c

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -1,25 +1,39 @@
-unsigned int    ft_strlen(char *str)
-{
-    unsigned int    i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/10 15:45:36 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/10 15:55:52 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
 }
 
-unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcpy(char *dest, char *str, unsigned int size)
 {
-    unsigned int    i;
-    unsigned int    src_len;
+	unsigned int	i;
 
-    src_len = ft_strlen(src);
-    i = 0;
-    while ((size > 0) && (i < size - 1) && (src[i] != '\0'))
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (src_len);
+	if (!dest || !str)
+		return (0);
+	if (size == 0)
+		return (ft_strlen(str));
+	i = 0;
+	while (str[i] && i < (size - 1))
+	{
+		dest[i] = str[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(str));
 }
