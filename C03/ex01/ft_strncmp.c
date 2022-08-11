da15c0d23d19bf9eb2b5f33773bcf719294c9f4f
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-11 19:30:13 +0200

    ft_strncmp.c

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -1,9 +1,21 @@
-int     ft_strncmp(char *s1, char *s2, unsigned int n)
-{
-    unsigned int     i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 19:27:17 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/11 19:29:57 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (i < n - 1 && s1[i] == s2[i])
-        i++;
-    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && i < n - 1)
+		i++;
+	return ((unsigned int)s1[i] - (unsigned int)s2[i]);
 }
