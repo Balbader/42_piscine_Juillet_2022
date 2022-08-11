3b025809036446f58b378116704a230fbc03cc10
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-11 19:51:45 +0200

    ft_strncat.c

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -1,25 +1,30 @@
-int     ft_strlen(char *str)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 19:50:00 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/11 19:51:32 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
-    int     i;
+	unsigned int	i;
+	unsigned int	j;
 
-    i = 0;
-    while (str[i] != '\0')
-        i++;
-    return (i);
-}
-
-char    *ft_strncat(char *dest, char *src, unsigned int nb)
-{
-    unsigned int i;
-    unsigned int j;
-
-    j = ft_strlen(dest);
-    i = 0;
-    while (src[i] && i < nb - 1)
-    {
-        dest[j] = src[i];
-        i++;
-    }
-    dest[j] = '\0';
-    return (dest);
+	i = 0;
+	j = 0;
+	while (dest[i])
+		i++;
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
