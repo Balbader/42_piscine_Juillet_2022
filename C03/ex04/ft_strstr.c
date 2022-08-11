4ec2b531123733c94a3e4ff3895297cb949479e6
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-11 19:59:06 +0200

    ft_strstr.c

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -1,19 +1,31 @@
-char    *ft_strstr(char *str, char *to_find)
-{
-    unsigned int i;
-    unsigned int j;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 19:55:39 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/11 19:58:25 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    i = 0;
-    while (str[i])
-    {
-        j = 0;
-        while (str[i] == to_find[j])
-        {
-            j++;
-            if (str[i] != to_find[j])
-                return (str + i);
-        }
-        i++;
-    }
-    return (0);
+char	*ft_strstr(char *str, char *to_find)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (str[i] == to_find[j])
+		{
+			j++;
+			if (str[i] != to_find[j])
+				return (str + i);
+		}
+		i++;
+	}
+	return (0);
 }
