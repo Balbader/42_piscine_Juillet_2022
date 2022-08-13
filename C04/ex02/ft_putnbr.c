f5d8742d9623babd626340ae014ad2b9acdfbe57
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-13 09:05:25 +0200

    ft_putnbr.c

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -1,33 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/13 08:59:01 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/13 09:04:53 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void    ft_print_neg(int nb)
+void	ft_putnbr(int nb)
 {
-    if (nb == -2147483648)
-    {
-        ft_putchar('-');
-        write(1, "2147483648", 11);
-    }
-}
-
-void    ft_putnbr(int nb)
-{
-    if (nb == -2147483648)
-        ft_print_neg(nb);
-    if (nb > -2147483648 && nb < 0)
-    {
-        ft_putchar('-');
-        nb *= -1;
-    }
-    if (nb >= 0 && nb < 10)
-        ft_putchar(nb + '0');
-    if (nb >= 10)
-    {
-        ft_putnbr(nb / 10);
-        ft_putnbr(nb % 10);
-    }
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (nb >= -2147483647 && nb < 0)
+	{
+		ft_putchar('-');
+		nb *= -1;
+	}
+	if (nb <= 9)
+		ft_putchar(nb + '0');
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
 }
